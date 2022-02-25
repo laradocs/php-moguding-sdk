f18e3945baab7200aecf2d1fabdebbbdcd3e52e5
laradocs <75692074@qq.com>
2022-02-26 00:12:33 +0800

    update

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -15,31 +15,31 @@ class ClientTest extends TestCase
 
     public function testLogin()
     {
-        $factory = Mockery::mock ( Client::class );
-        $factory->shouldReceive ( 'login' )->andReturn ( json_decode ( file_get_contents ( __DIR__ . '/login.json' ), true ) );
+        $factory = Mockery::mock(Client::class);
+        $factory->shouldReceive('login')->andReturn(json_decode(file_get_contents(__DIR__ . '/login.json'), true));
         $data = $factory->login('xxx', 'xxx', 'xxx');
 
-        $this->assertNotEmpty ( $data );
-        $this->assertSame ( 200, $data [ 'code' ] );
+        $this->assertNotEmpty($data);
+        $this->assertSame(200, $data ['code']);
     }
 
     public function testGetPlan()
     {
-        $factory = Mockery::mock ( Client::class );
-        $factory->shouldReceive ( 'getPlan' )->andReturn ( json_decode ( file_get_contents ( __DIR__ . '/get_plan_by_stu.json' ), true ) );
-        $data = $factory->getPlan('123456', 'student', '10000' );
+        $factory = Mockery::mock(Client::class);
+        $factory->shouldReceive('getPlan')->andReturn(json_decode(file_get_contents(__DIR__ . '/get_plan_by_stu.json'), true));
+        $data = $factory->getPlan('123456', 'student', '10000');
 
-        $this->assertSame ( 200, $data [ 'code' ] );
+        $this->assertSame(200, $data ['code']);
     }
 
     public function testSave()
     {
-        $factory = Mockery::mock ( Client::class );
-        $factory->shouldReceive ( 'save' )->andReturn ( json_decode ( file_get_contents ( __DIR__ . '/save.json' ), true ) );
-        $data = $factory->save ( '123456', '10000', 'xxx', 'xxx', 'xxxxxxxxxx', 100.000000, 10.000000, 'xxx', 'xxxx', '987654' );
+        $factory = Mockery::mock(Client::class);
+        $factory->shouldReceive('save')->andReturn(json_decode(file_get_contents(__DIR__ . '/save.json'), true));
+        $data = $factory->save('123456', '10000', 'xxx', 'xxx', 'xxxxxxxxxx', 100.000000, 10.000000, 'xxx', 'xxxx', '987654');
 
-        $this->assertNotEmpty ( $data );
-        $this->assertSame ( 200, $data [ 'code' ] );
+        $this->assertNotEmpty($data);
+        $this->assertSame(200, $data ['code']);
     }
 
 
@@ -48,8 +48,8 @@ class ClientTest extends TestCase
      */
     public function testSctSend()
     {
-        $factory = Mockery::mock ( Client::class );
-        $factory->shouldReceive ( 'sctSend' );
-        $factory->sctSend ( 'xxx', '标题', '后文' );
+        $factory = Mockery::mock(Client::class);
+        $factory->shouldReceive('sctSend');
+        $factory->sctSend('xxx', '标题', '后文');
     }
 }
