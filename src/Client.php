b673c2e0b6f38459b9259a4aeefff82e8485339d
laradocs <75692074@qq.com>
2022-03-08 05:33:14 +0800

    2.1.3

--- src/Client.php
+++ src/Client.php
@@ -5,6 +5,7 @@ namespace Laradocs\Moguding;
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Psr7\Response;
 use Laradocs\Exceptions\SendKeyInvalidException;
+use Laradocs\Moguding\Exceptions\RequestTimeoutException;
 use Laradocs\Moguding\Exceptions\TokenExpiredException;
 use GuzzleHttp\Client as Guzzle;
 use Laradocs\Moguding\Exceptions\UnauthenticatedException;
@@ -40,23 +41,25 @@ class Client
     /**
      * 用户登录
      *
-     * @param string $driver
+     * @param string $device
      * @param string $phone
      * @param string $password
      * @return array
-     * @throws \GuzzleHttp\Exception\GuzzleException
-     * @throws \JsonException
      */
     public function login(string $device, string $phone, string $password): array
     {
-        $response = $this->client()
-            ->post('session/user/v1/login', [
-                'json' => [
-                    'loginType' => $device,
-                    'phone' => $phone,
-                    'password' => $password,
-                ],
-            ]);
+        try {
+            $response = $this->client()
+                ->post('session/user/v1/login', [
+                    'json' => [
+                        'loginType' => $device,
+                        'phone' => $phone,
+                        'password' => $password,
+                    ],
+                ]);
+        } catch ( GuzzleException ) {
+            throw new RequestTimeoutException ( '登录请求超时。' );
+        }
 
         return $this->body($response);
     }
@@ -68,21 +71,24 @@ class Client
      * @param string $userType
      * @param int $userId
      * @return array
-     * @throws \GuzzleHttp\Exception\GuzzleException
-     * @throws \JsonException
      */
     public function getPlan(string $token, string $userType, int $userId): array
     {
-        $response = $this->client()
-            ->post('practice/plan/v3/getPlanByStu', [
-                'headers' => [
-                    'authorization' => $token,
-                ],
-                'json' => [
-                    'roleKey' => $userType,
-                    'sign' => md5(sprintf('%d%s%s', $userId, $userType, $this->salt)),
-                ],
-            ]);
+        try {
+            $response = $this->client()
+                ->post('practice/plan/v3/getPlanByStu', [
+                    'headers' => [
+                        'authorization' => $token,
+                    ],
+                    'json' => [
+                        'roleKey' => $userType,
+                        'sign' => md5(sprintf('%d%s%s', $userId, $userType, $this->salt)),
+                    ],
+                ]);
+        } catch ( GuzzleException ) {
+            throw new RequestTimeoutException('获取计划请求超时。');
+        }
+
 
         return $this->body($response);
     }
@@ -93,7 +99,7 @@ class Client
      * @param string $token
      * @param int $userId
      * @param string $province
-     * @param string $city
+     * @param string|null $city
      * @param string $address
      * @param float $longitude
      * @param float $latitude
@@ -103,8 +109,6 @@ class Client
      * @param string|null $description
      * @param string $country
      * @return array
-     * @throws \GuzzleHttp\Exception\GuzzleException
-     * @throws \JsonException
      */
     public function save(string $token, int $userId, string $province, ?string $city, string $address, float $longitude, float $latitude, string $type, string $device, string $planId, ?string $description = null, string $country = '中国'): array
     {
@@ -112,25 +116,29 @@ class Client
             $city = $province;
         }
         $address = sprintf('%s%s%s%s', $country, $province, (($city === $province) ? '' : $city), $address);
-        $response = $this->client()
-            ->post('attendence/clock/v2/save', [
-                'headers' => [
-                    'authorization' => $token,
-                    'sign' => md5(sprintf('%s%s%s%d%s%s', ucfirst($device), $type, $planId, $userId, $address, $this->salt)),
-                ],
-                'json' => [
-                    'country' => $country,
-                    'province' => $province,
-                    'city' => $city,
-                    'address' => $address,
-                    'longitude' => $longitude,
-                    'latitude' => $latitude,
-                    'type' => $type,
-                    'device' => ucfirst($device),
-                    'planId' => $planId,
-                    'description' => $description,
-                ],
-            ]);
+        try {
+            $response = $this->client()
+                ->post('attendence/clock/v2/save', [
+                    'headers' => [
+                        'authorization' => $token,
+                        'sign' => md5(sprintf('%s%s%s%d%s%s', ucfirst($device), $type, $planId, $userId, $address, $this->salt)),
+                    ],
+                    'json' => [
+                        'country' => $country,
+                        'province' => $province,
+                        'city' => $city,
+                        'address' => $address,
+                        'longitude' => $longitude,
+                        'latitude' => $latitude,
+                        'type' => $type,
+                        'device' => ucfirst($device),
+                        'planId' => $planId,
+                        'description' => $description,
+                    ],
+                ]);
+        } catch ( GuzzleException ) {
+            throw new RequestTimeoutException('打卡保存请求超时。');
+        }
 
         return $this->body($response);
     }
@@ -140,7 +148,6 @@ class Client
      *
      * @param Response $response
      * @return array
-     * @throws \JsonException
      */
     protected function body(Response $response): array
     {
@@ -160,7 +167,6 @@ class Client
      * @param string $title
      * @param string|null $desp
      * @return void
-     * @throws GuzzleException
      */
     public function sctSend(?string $sendKey, string $title, ?string $desp = null): void
     {
