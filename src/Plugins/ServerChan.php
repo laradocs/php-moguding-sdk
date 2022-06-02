1659b37622853781320be30dcc4c152441c998fb
Tall Libra <75692074@qq.com>
2022-06-02 09:46:14 +0800

    修复推送通知异常

--- src/Plugins/ServerChan.php
+++ src/Plugins/ServerChan.php
@@ -84,7 +84,7 @@ class ServerChan extends Channel
         try {
             $response = $this->getHttpClient()
                 ->post($uri, [
-                    RequestOptions::FORM_PARAMS => [
+                    RequestOptions::QUERY => [
                         'title' => $this->title,
                         'desp' => $this->desp,
                         'channel' => $this->channel,
